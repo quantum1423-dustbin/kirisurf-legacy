46ea0d51d196f8c03a925db06ef3a63b58a6db8a
Yuhao Dong <yd2dong@uwaterloo.ca>
2014-04-04 20:57:00 -0400

    qjwneklfjsndmfsd

--- e2e_structs.go
+++ e2e_structs.go
@@ -66,14 +66,14 @@ func (wire *gobwire) Receive() (e2e_segment, error) {
 func (wire *gobwire) Send(thing e2e_segment) error {
 	wire._slock.Lock()
 	defer wire._slock.Unlock()
-	log.Debug("Sending: ", thing)
+	//log.Debug("Sending: ", thing)
 	tosend := make([]byte, len(thing.Body)+1+2+2)
 	tosend[0] = byte(thing.Flag)
 	binary.LittleEndian.PutUint16(tosend[1:3], uint16(thing.Connid))
 	binary.LittleEndian.PutUint16(tosend[3:5], uint16(len(thing.Body)))
 	copy(tosend[5:], thing.Body)
 	_, err := wire.conn.Write(tosend)
-	log.Debug("Sent.")
+	//log.Debug("Sent.")
 	return err
 }
 
